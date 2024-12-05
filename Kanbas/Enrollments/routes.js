2e619c757fd40d058ef380739b613fae5cbd3ab5
Aryan Kale <aryankale22@gmail.com>
2024-12-04 21:41:03 -0500

    5.3.4

--- Kanbas/Enrollments/routes.js
+++ Kanbas/Enrollments/routes.js
@@ -1,9 +1,9 @@
 import * as enrollmentsDao from "./dao.js";
 
 export default function EnrollmentRoutes(app) {
-    app.get("/api/enrollments/:userId", async (req, res) => {
+  app.get("/api/enrollments/:userId", async (req, res) => {
     const { userId } = req.params;
-    const enrollments = await enrollmentsDao.getEnrollmentsForUser(userId);
+    const enrollments = await enrollmentsDao.findCoursesForUser(userId);
     res.send(enrollments);
   });
   app.post("/api/enrollments", async (req, res) => {
