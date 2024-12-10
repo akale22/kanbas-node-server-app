b67ff4e9842d834ba8ac960be33abeef65e5f43b
Aryan Kale <aryankale22@gmail.com>
2024-12-10 11:16:18 -0500

    updated dao lol - forgot

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -14,3 +14,6 @@ export async function findUsersForCourse(courseId) {
   const enrollments = await model.find({ course: courseId }).populate("user");
   return enrollments.map((enrollment) => enrollment.user);
 }
+export async function removeEnrollmentsForCourse(courseId) {
+  return model.deleteMany({ course: courseId });
+}
